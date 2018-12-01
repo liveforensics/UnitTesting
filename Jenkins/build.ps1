77df3120f35e04d43e66415fd1dc4f52332506db
mark <liveforensics@mark-morley.co.uk>
2018-12-01 16:50:25 +0000

    cx

--- Jenkins/build.ps1
+++ Jenkins/build.ps1
@@ -8,6 +8,8 @@ Push-Location
 Get-Location
 
 # bat "\"${tool 'VS2017'}\" MyClasses.sln /p:Configuration=Debug /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
-msbuild MyClasses.sln /p:Configuration=Debug /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.4
+# msbuild MyClasses.sln /p:Configuration=Debug /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.4
+
+Invoke-Expression -Command "msbuild myclasses.sln"
 
 Pop-Location
