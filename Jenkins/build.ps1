2a0b596a987eaec68ecb8057960fa7f1f8cb5dd1
mark <liveforensics@mark-morley.co.uk>
2018-12-02 10:14:05 +0000

    dsa

--- Jenkins/build.ps1
+++ Jenkins/build.ps1
@@ -13,9 +13,11 @@ Write-Host "Restoring Nuget Packages"
 nuget restore
 
 Write-Host "Building the Project"
-Invoke-Expression -Command "msbuild myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber"
+# Invoke-Expression -Command "msbuild myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber"
 # msbuild myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber | Out-Host
 
+$Result = (Start-Process -FilePath 'msbuild.exe' -ArgumentList "myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber" -Wait -NoNewWindow -PassThru).ExitCode
+$Result
 if($lastexitcode){
     Write-Host "EXITTCODE = $lastexitcode"
 }else{
