53af4871150f2187e5f2bc49ca1758737ac3b3a6
mark <liveforensics@mark-morley.co.uk>
2018-12-02 10:10:58 +0000

    vxc

--- Jenkins/build.ps1
+++ Jenkins/build.ps1
@@ -13,9 +13,13 @@ Write-Host "Restoring Nuget Packages"
 nuget restore
 
 Write-Host "Building the Project"
-# Invoke-Expression -Command "msbuild myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber"
-msbuild myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber | Out-Host
+Invoke-Expression -Command "msbuild myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber"
+# msbuild myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber | Out-Host
 
-Write-Host "EXIT CODE = $LASTTEXITCODE"
+if($lastexitcode){
+    Write-Host "EXITTCODE = $lastexitcode"
+}else{
+   Write-Host 'No Error reported'
+}
 
 Pop-Location
