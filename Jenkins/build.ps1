f03b5c662ea38fed12f5398a663c30dfafa5dbe6
mark <liveforensics@mark-morley.co.uk>
2018-12-02 11:07:02 +0000

    cxz

--- Jenkins/build.ps1
+++ Jenkins/build.ps1
@@ -14,30 +14,10 @@ nuget restore
 
 Write-Host "Building the Project"
 # Invoke-Expression -Command "msbuild myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber"
-# msbuild myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber | Out-Host
+msbuild myclasses.sln /p:Configuration=Debug /p:Platform='Any CPU' /p:ProductVersion=1.0.0.$BuildNumber | Out-File c:\persist\buildresults.log
 # $Result = (Start-Process -FilePath 'msbuild.exe' -ArgumentList 'myclasses.sln /p:Configuration=Debug /p:ProductVersion=1.0.0.$BuildNumber /p:Platform="Any CPU"' -Wait -PassThru).ExitCode
 
-$proc = Start-Process -FilePath 'msbuild.exe' -ArgumentList 'myclasses.sln /p:Configuration=Debug /p:ProductVersion=1.0.0.$BuildNumber /p:Platform="Any CPU"' -Wait -PassThru
-$timeouted = $null
-$proc | Wait-Process -Timeout 15 -ErrorAction SilentlyContinue -ErrorVariable timeouted
-if ($timeouted)
-{
-    # terminate the process
-    $proc | Stop-Process
-    Write-Host "TIME OUT happened"
-    # update internal error counter
-}
-elseif ($proc.ExitCode -ne 0)
-{
-    Pop-Location | Out-Null
-    exit 1
-}
 
-# if($Result){
-#     Write-Host "EXITTCODE = $Result"
-# }else{
-#    Write-Host 'No Exit Code reported'
-# }
 
 Pop-Location | Out-Null
 
