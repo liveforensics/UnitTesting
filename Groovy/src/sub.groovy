3dcf27443a5a87248dc3a12e5a4bfd3e9de6ef71
mark <liveforensics@mark-morley.co.uk>
2018-12-05 22:01:55 +0000

    delete loads of stuff

--- Groovy/src/sub.groovy
+++
@@ -1,8 +0,0 @@
-// This is the 'include file'
-// Testmain.groovy will load it as an implicit class
-// Each method in here will become a method on the implicit class
-class sub {
-    def myUtilityMethod(String msg) {
-        println "myUtilityMethod running with: ${msg}"
-    }
-}
