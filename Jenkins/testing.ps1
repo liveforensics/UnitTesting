5a275c1cb07912b209eb164acb54ca00a516fba5
mark <liveforensics@mark-morley.co.uk>
2018-12-05 20:30:12 +0000

    vcx

--- Jenkins/testing.ps1
+++ Jenkins/testing.ps1
@@ -10,11 +10,14 @@ Set-Location $location
 
 vstest.console.exe MyClassesTest.dll /Logger:trx /ResultsDirectory:c:\\persist
 
-Get-ChildItem . -File -Recurse | Where-Object {$_.Extension -eq '.coverage'; } | ForEach-Object {
-    Write-Host "Found: " $_.FullName
-}
+# Get-ChildItem . -File -Recurse | Where-Object {$_.Extension -eq '.coverage'; } | ForEach-Object {
+#     Write-Host "Found: " $_.FullName
+# }
 
-codecoverage.exe collect /output:test.coverage .\MyClassesTest.dll
+$args = "collect /output:test.coverage .\MyClassesTest.dll"
+
+Start-Process -FilePath codecoverage.exe -ArgumentList $args -Wait -PassThru -Verb runAs 
+#codecoverage.exe collect /output:test.coverage .\MyClassesTest.dll
 codecoverage.exe analyze /output:test.coveragexml .\test.coverage
 
 Pop-Location | Out-Null
