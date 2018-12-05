4f6431c8c285d1e629af9162f9d6acfb936b2394
mark <liveforensics@mark-morley.co.uk>
2018-12-05 21:31:10 +0000

    fsa

--- Jenkins/testing.ps1
+++ Jenkins/testing.ps1
@@ -4,21 +4,23 @@ Push-Location | Out-Null
 
 $location = Get-Location
 
+$results = Join-Path $location "RealTestResults"
+
 $location = Join-Path $location 'MyClassesTest\\bin\\debug'
 
 Set-Location $location
 
 vstest.console.exe MyClassesTest.dll /Logger:trx /Enablecodecoverage
 
-# Get-ChildItem . -File -Recurse | Where-Object {$_.Extension -eq '.coverage'; } | ForEach-Object {
-#     Write-Host "Found: " $_.FullName
-# }
+Get-ChildItem . -File -Recurse | Where-Object {$_.Extension -eq '.coverage'; } | ForEach-Object {
+    Write-Host "Found: " $_.FullName
+}
 
-$args = "collect /output:test.coverage .\\MyClassesTest.dll"
+# $args = "collect /output:test.coverage .\\MyClassesTest.dll"
 
-Start-Process -FilePath codecoverage.exe -ArgumentList $args -Wait -PassThru -Verb runAs 
+# Start-Process -FilePath codecoverage.exe -ArgumentList $args -Wait -PassThru -Verb runAs 
 #codecoverage.exe collect /output:test.coverage .\MyClassesTest.dll
-codecoverage.exe analyze /output:test.coveragexml .\test.coverage
+# codecoverage.exe analyze /output:test.coveragexml .\test.coverage
 
 Pop-Location | Out-Null
 
